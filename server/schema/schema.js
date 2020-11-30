b62491adeae35f10c13a83897c21d44c8888c608
Kumar Nikhil <knikhil352000@gmail.com>
2020-12-01 03:16:48 +0530

    Passing Null Object Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,9 @@ const {
     GraphQLSchema, 
     GraphQLID, 
     GraphQLInt,
-    GraphQLList } = graphql;
+    GraphQLList,
+    GraphQLNonNull
+ } = graphql;
 
 // dummy data
 // var books = [
@@ -122,8 +124,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor:{
             type: AuthorType,
             args:{
-                name:{type: GraphQLString},
-                age: {type: GraphQLInt}
+                name:{type:new GraphQLNonNull (GraphQLString)},
+                age: {type:new GraphQLNonNull (GraphQLInt)}
             },
             resolve(parent, args){
                 let author = new Author({
@@ -136,9 +138,9 @@ const Mutation = new GraphQLObjectType({
         addBook:{
             type: BookType,
             args:{
-                name: {type: GraphQLString},
-                genre: {type: GraphQLString},
-                authorId: {type: GraphQLID},
+                name: {type: new GraphQLNonNull (GraphQLString)},
+                genre: {type:new GraphQLNonNull (GraphQLString)},
+                authorId: {type:new GraphQLNonNull (GraphQLID)},
             },
             resolve(parent, args){
                 let book = new Book({
@@ -192,3 +194,18 @@ module.exports = new GraphQLSchema({
 //     }
 //   }
 // }
+
+// mutation{
+//     addBook(name: "Name of the Wind", genre: "Fantasty"){
+      
+//     }
+//   }
+// {
+//     author(id: "5fc563403247f027dc3a62b0"){
+//       name
+//       age
+//       books{
+//         name
+//       }
+//     }
+//   }
