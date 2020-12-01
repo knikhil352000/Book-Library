f41bda64479bc6c53b09dcd9a25847f4e821e231
Kumar Nikhil <knikhil352000@gmail.com>
2020-12-02 05:06:12 +0530

    Rendering data from the frontend

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+
+// allow origin requests
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/usersDB',{ useNewUrlParser: true , useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
