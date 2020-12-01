74cf8c7f7f14345368c8b142c63e5530dada34e0
Kumar Nikhil <knikhil352000@gmail.com>
2020-12-02 02:24:54 +0530

    Integrating Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
-import BookList from './component/BookList';
-class App extends Component{
-  render(){
+import React, {Component} from "react";
+import {ApolloClient, ApolloProvider} from "@apollo/client";
+import BookList from "./component/BookList";
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
+class App extends Component {
+  render() {
     return (
-      <div id="main">
-        <h1>Nikhil Singh</h1>
-        <BookList />
-      </div>
-    )
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Nikhil Singh</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
+    );
   }
 }
 export default App;
